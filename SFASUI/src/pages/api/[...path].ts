c55558fc716196979e26d412fd415319ff9c2bfe
krendeleno <krendeleno@gmail.com>
2023-01-29 18:55:36 +0300

    fix: фикс миддлварки

--- SFASUI/src/pages/api/[...path].ts
+++ SFASUI/src/pages/api/[...path].ts
@@ -1,10 +1,16 @@
-import httpProxy from 'http-proxy'
+import { IncomingMessage, ServerResponse } from 'http'
+import httpProxy from "http-proxy";
 
-const API_URL = `https://sportsmap.spb.ru/api`
-const proxy = httpProxy.createProxyServer()
+const API_URL = `https://sportsmap.spb.ru`;
+const proxy = httpProxy.createProxyServer();
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
 
-export default (req, res) => {
-  return new Promise((resolve, reject) => {
+export default async (req: IncomingMessage, res: ServerResponse<IncomingMessage>) => {
+  return await new Promise<void>((resolve, reject) => {
     proxy.web(req, res, { target: API_URL, changeOrigin: true }, (err) => {
       if (err) {
         return reject(err)
