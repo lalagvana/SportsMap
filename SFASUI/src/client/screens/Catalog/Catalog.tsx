8594188503d812ebc2123739f600bb1d123f774f
krendeleno <krendeleno@gmail.com>
2023-05-01 23:08:47 +0800

    fix: фикс импорта

--- SFASUI/src/client/screens/Catalog/Catalog.tsx
+++ SFASUI/src/client/screens/Catalog/Catalog.tsx
@@ -4,7 +4,7 @@ import { Header } from 'src/client/shared/components/Header';
 import { Footer } from 'src/client/shared/components/Footer';
 import { AnimationReveal } from 'src/client/shared/components/AnimationReveal';
 
-import { Filters } from "./components/Filters";
+import { Filters } from "./components/Filters/Filters";
 
 export const Catalog = () => {
     return (
