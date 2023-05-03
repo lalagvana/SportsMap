267fbdf9e9393feae25cc78b84460222c73e5521
krendeleno <krendeleno@gmail.com>
2023-05-03 22:37:28 +0800

    chore: удалена лишняя страница с контактами

--- SFASUI/src/client/screens/ContactUs/components/Form/Form.helpers.ts
+++
@@ -1,9 +0,0 @@
-import { EmailSend } from 'src/client/shared/utils/api/emails';
-
-import { FormFields } from '.';
-
-export const createEmailRequest = (fields: FormFields): EmailSend.Body => ({
-    fullName: fields.name,
-    email: fields.email,
-    text: fields.message,
-});
